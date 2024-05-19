728ef6081632a98fc58e0c4e736ce08396056192
Kauã Xavier <kauaxavier032@gmail.com>
2024-05-19 17:03:33 -0300

    feat: "Added search input, table, and styling to ListRemoteStudents component"

--- src/pages/ListRemoteStudents/ListRemoteStudents.jsx
+++ src/pages/ListRemoteStudents/ListRemoteStudents.jsx
@@ -1,12 +1,52 @@
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
-import { MainRemoteStudents } from "./ListRemoteStudentsStyled";
+import {
+  ListAreaRemoteStudents,
+  MainRemoteStudents,
+} from "./ListRemoteStudentsStyled";
 
 export default function ListRemoteStudent() {
   return (
     <MainRemoteStudents>
       <Navbar p3={1} />
       <Header />
+      <ListAreaRemoteStudents>
+        <div>
+          <input id="searchStudent" type="text" placeholder="Pesquisar..." />
+          <button></button>
+        </div>
+        <div id="listArea">
+          <h2>Lista de Estudantes Remota</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Nº</th>
+                <th>Nome</th>
+                <th>Telefone</th>
+                <th>Responsável</th>
+                <th>Nascimento</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Nº</td>
+                <td>Nome</td>
+                <td>Telefone</td>
+                <td>Responsável</td>
+                <td>Nascimento</td>
+                <td>
+                  <button className="editButton"></button>
+                </td>
+                <td>
+                  <button className="deleteButton"></button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </ListAreaRemoteStudents>
     </MainRemoteStudents>
   );
 }
--- src/pages/ListRemoteStudents/ListRemoteStudentsStyled.jsx
+++ src/pages/ListRemoteStudents/ListRemoteStudentsStyled.jsx
@@ -5,3 +5,70 @@ export const MainRemoteStudents = styled.main`
   flex-direction: column;
   align-items: center;
 `;
+
+export const ListAreaRemoteStudents = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+
+  input {
+    width: 360px;
+    height: 40px;
+    border-radius: 100px;
+    border: 1px solid #bababa;
+    padding-left: 10px;
+  }
+  #listArea {
+    width: 720px;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    background-color: white;
+    padding-top: 20px;
+    border-radius: 15px;
+    border: 1px solid #bababa;
+  }
+  div h2 {
+    font-size: 20px;
+    font-weight: bold;
+    color: #454545;
+  }
+  table {
+    width: 100%;
+  }
+  table thead {
+    font-size: 18px;
+    font-weight: bold;
+    color: #454545;
+  }
+  table {
+    text-align: center;
+  }
+  table tbody tr {
+    border-top: 1px solid #bababa;
+  }
+  table thead tr th,
+  table tbody tr td {
+    padding: 10px;
+  }
+  table .editButton,
+  table .deleteButton {
+    background-repeat: no-repeat;
+    background-size: cover;
+    border: none;
+    background-color: white;
+  }
+  table .editButton {
+    width: 10px;
+    height: 10px;
+    background-image: url("./editIcon.png");
+  }
+  table .deleteButton {
+    width: 15px;
+    height: 16.67px;
+    background-image: url("./deleteIcon.png");
+  }
+`;
