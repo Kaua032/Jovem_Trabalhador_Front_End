f53724aa54b00c5ea63f337a1b611aa62d1b0129
Kauã Xavier <kauaxavier032@gmail.com>
2024-05-23 14:59:46 -0300

    feat: "Added state and useEffect to ListLocalStudents component, implemented delete student functionality, and styled the list area and table"

--- src/pages/ListLocalStudents/ListLocalStudents.jsx
+++ src/pages/ListLocalStudents/ListLocalStudents.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import Header from "../../components/Header/Header";
 import Navbar from "../../components/Navbar/Navbar";
 import {
@@ -6,12 +7,78 @@ import {
 } from "./ListLocalStudentsStyled";
 
 export default function ListLocalStudents() {
+  const [infoLocalStudents, setInfoLocalStudents] = useState([]);
+
+  function handleDeleteStudent(index) {
+    const currentStudents = localStorage.getItem("students")
+      ? JSON.parse(localStorage.getItem("students"))
+      : [];
+
+    currentStudents.splice(index, 1);
+
+    localStorage.setItem("students", JSON.stringify(currentStudents));
+
+    setInfoLocalStudents(currentStudents);
+  }
+
+  function calculateAge(birthDate) {
+    const today = new Date();
+    const birthDateObj = new Date(birthDate);
+    let age = today.getFullYear() - birthDateObj.getFullYear();
+    const monthDifference = today.getMonth() - birthDateObj.getMonth();
+    if (
+      monthDifference < 0 ||
+      (monthDifference === 0 && today.getDate() < birthDateObj.getDate())
+    ) {
+      age--;
+    }
+    return age;
+  }
+
+  useEffect(() => {
+    setInfoLocalStudents(
+      localStorage.getItem("students")
+        ? JSON.parse(localStorage.getItem("students"))
+        : []
+    );
+  }, []);
   return (
     <MainLocalStudents>
       <Navbar p2={1} />
       <Header />
       <ListAreaLocalStudents>
-        <div id="listArea"></div>
+        <div id="listArea">
+          <h2>Lista de Estudantes Local</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Nº</th>
+                <th>Nome</th>
+                <th>Telefone</th>
+                <th>Responsável</th>
+                <th>Idade</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {infoLocalStudents.map((student, index) => (
+                <tr key={index}>
+                  <td>{index + 1}º</td>
+                  <td>{student.name}</td>
+                  <td>{student.phone}</td>
+                  <td>{student.responsible_name}</td>
+                  <td>{calculateAge(student.born_date)}</td>
+                  <td>
+                    <button
+                      className="deleteButton"
+                      onClick={() => handleDeleteStudent(index)}
+                    ></button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </ListAreaLocalStudents>
     </MainLocalStudents>
   );
--- src/pages/ListLocalStudents/ListLocalStudentsStyled.jsx
+++ src/pages/ListLocalStudents/ListLocalStudentsStyled.jsx
@@ -11,4 +11,50 @@ export const ListAreaLocalStudents = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 30px;
+
+  #listArea {
+    width: 720px;
+    min-height: 571px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+    background-color: white;
+    padding-top: 20px;
+    border-radius: 15px;
+    border: 1px solid #bababa;
+  }
+  div h2 {
+    font-size: 20px;
+    font-weight: bold;
+    color: #454545;
+  }
+  table {
+    width: 100%;
+  }
+  table thead {
+    font-size: 18px;
+    font-weight: bold;
+    color: #454545;
+  }
+  table {
+    text-align: center;
+  }
+  table tbody tr {
+    border-top: 1px solid #bababa;
+  }
+  table thead tr th,
+  table tbody tr td {
+    padding: 10px;
+  }
+
+  .deleteButton {
+    border: none;
+    width: 15px;
+    height: 16.67px;
+    background-image: url("./deleteIcon.png");
+    background-color: white;
+    background-size: cover;
+    background-repeat: no-repeat;
+  }
 `;
