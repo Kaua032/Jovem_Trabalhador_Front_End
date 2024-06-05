6ab580c18e90cd6c9626ee6247e258ffc569193d
Kauã Xavier <kauaxavier032@gmail.com>
2024-06-05 16:27:37 -0300

    feat: Added ToastContainer and notification functionality to CollegeList component, modified handleInputChange function to check for local storage students, and reformatted JSX structure.

--- src/components/CollegesList/CollegeList.jsx
+++ src/components/CollegesList/CollegeList.jsx
@@ -1,9 +1,22 @@
 import { useEffect, useState } from "react";
 import { ListArea } from "./CollegeListStyled";
 import { getAllColleges, updateCollege } from "../../services/collegeService";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function CollegeList() {
   const [allColleges, setAllColleges] = useState([]);
+
+  const ToastNotice = (message, type) =>
+    toast[type](`${message}`, {
+      position: "top-center",
+      autoClose: 1500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "colored",
+    });
 
   async function findAllColleges() {
     try {
@@ -16,68 +29,76 @@ export default function CollegeList() {
   }
 
   const handleInputChange = async (index, field, value) => {
+    const students = JSON.parse(localStorage.getItem("students") || "[]");
+    if (students.length > 0) {
+      return ToastNotice(
+        "Por favor, adicione todos os alunos da rede local.",
+        "error"
+      );
+    }
     const updatedColleges = [...allColleges];
     updatedColleges[index][field] = value;
     setAllColleges(updatedColleges);
-    const response = await updateCollege(allColleges[index]._id, {
+    await updateCollege(allColleges[index]._id, {
       [field]: allColleges[index][field],
     });
-
-    console.log(response)
   };
 
   useEffect(() => {
     findAllColleges();
   }, []);
   return (
-    <ListArea>
-      <div>
-        <h2>Instituições na Rede Remota</h2>
-        <table id="remota">
-          <thead>
-            <tr>
-              <th>Nº</th>
-              <th>Nome</th>
-              <th>Cidade</th>
-              <th>UF</th>
-            </tr>
-          </thead>
-          <tbody>
-            {allColleges.map((college, index) => (
-              <tr key={index}>
-                <td>{index + 1}º</td>
-                <td>
-                  <input
-                    type="text"
-                    value={college.name}
-                    onChange={(e) =>
-                      handleInputChange(index, "name", e.target.value)
-                    }
-                  />
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    value={college.city}
-                    onChange={(e) =>
-                      handleInputChange(index, "city", e.target.value)
-                    }
-                  />
-                </td>
-                <td>
-                  <input
-                    type="text"
-                    value={college.uf}
-                    onChange={(e) =>
-                      handleInputChange(index, "uf", e.target.value)
-                    }
-                  />
-                </td>
+    <>
+      <ListArea>
+        <div>
+          <h2>Instituições na Rede Remota</h2>
+          <table id="remota">
+            <thead>
+              <tr>
+                <th>Nº</th>
+                <th>Nome</th>
+                <th>Cidade</th>
+                <th>UF</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </ListArea>
+            </thead>
+            <tbody>
+              {allColleges.map((college, index) => (
+                <tr key={index}>
+                  <td>{index + 1}º</td>
+                  <td>
+                    <input
+                      type="text"
+                      value={college.name}
+                      onChange={(e) =>
+                        handleInputChange(index, "name", e.target.value)
+                      }
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      value={college.city}
+                      onChange={(e) =>
+                        handleInputChange(index, "city", e.target.value)
+                      }
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="text"
+                      value={college.uf}
+                      onChange={(e) =>
+                        handleInputChange(index, "uf", e.target.value)
+                      }
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </ListArea>
+      <ToastContainer />
+    </>
   );
 }
