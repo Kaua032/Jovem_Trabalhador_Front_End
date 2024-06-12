aaccc219767aa5cd1a8a00dc66bfbe7909dd7bea
Kauã Xavier <kauaxavier032@gmail.com>
2024-06-12 10:17:29 -0300

    feat: "VitePWA created"

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "prompt",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+  manifest: {
+    name: "Jovem Trabalhador",
+    short_name: "Jovem Trabalhador",
+    description: "Um app para registrar alunos de maneira simples e eficiente.",
+    icons: [
+      {
+        src: "./android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "./android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+    ],
+    theme_color: "#0A7DE8",
+    background_color: "#ffffff",
+    display: "fullscreen",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), VitePWA(manifestForPlugin)],
+});
