4a4870292205d2d67b619ce095870f37ba219d0a
Kauã Xavier <kauaxavier032@gmail.com>
2024-06-21 15:49:42 -0300

    feat: "Updated text of two NavbarComponent instances in Navbar.jsx"

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,8 +9,8 @@ export default function Navbar({ ...props }) {
       </a>
       <div>
         <NavbarComponent href="/" type={props.p1} name="Adicionar Aluno" />
-        <NavbarComponent href="/localStudents" type={props.p2} name="Alunos Local" />
-        <NavbarComponent href="/remoteStudents" type={props.p3} name="Alunos Remota" />
+        <NavbarComponent href="/localStudents" type={props.p2} name="Adicionar Alunos Banco" />
+        <NavbarComponent href="/remoteStudents" type={props.p3} name="Alunos Salvos no Banco" />
         <NavbarComponent href="/college" type={props.p4} name="Instituições" />
         <NavbarComponent href="/courses" type={props.p5} name="Cursos" />
         <NavbarComponent href="/party" type={props.p6} name="Turmas" />
